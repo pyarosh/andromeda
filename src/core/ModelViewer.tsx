d0ab1ec114f498927c4d5df8cb43f1a522c4c336
pyarosh <peteryarosh@gmail.com>
2022-04-21 19:08:25 -0400

    initial ModelViewer w/ default lighting model

---
+++ src/core/ModelViewer.tsx
@@ -0,0 +1,91 @@
+import * as THREE from 'three';
+import React, {
+	Fragment,
+	useEffect,
+	useRef
+} from 'react';
+import {
+	Canvas
+} from '@react-three/fiber';
+
+type ModelViewerProps = {
+	models?: (() => JSX.Element)[]
+	Lighting?: () => JSX.Element
+	Camera?: () => JSX.Element
+};
+export function ModelViewer({ models, Lighting, Camera } : ModelViewerProps) {
+
+	return (
+		<div 
+			style={{
+				position: 'absolute',
+				height: '100%',
+				width: '100%',
+				backgroundColor: '#cccccc'
+			}}
+		>
+
+			<Canvas
+				shadows={true}
+				dpr={window.devicePixelRatio}
+				onCreated={state => state.gl.toneMapping = THREE.ACESFilmicToneMapping}
+			>
+
+				<scene >
+
+					{ Lighting ? <Lighting /> : <DefaultLights /> }
+					{ Camera ? <Camera /> : null }
+					{ models && models.map(Model => <Model />) }
+					
+				</scene>
+			</Canvas>
+		</div>
+	);
+}
+
+function DefaultLights(): JSX.Element {
+
+	const spotLightRef1 = useRef<THREE.SpotLight>(null);
+
+	useEffect(() => {
+		if (spotLightRef1.current) {
+
+			if (spotLightRef1.current.shadow.map) {
+				spotLightRef1.current.shadow.bias = -0.0001;
+				spotLightRef1.current.shadow.map.height = 1024*4;
+				spotLightRef1.current.shadow.map.width = 1024*4;
+			}	
+		}
+	}, [ spotLightRef1 ]);
+
+	return (
+		<Fragment>
+			<hemisphereLight
+				intensity={0.5}
+			/>
+			<spotLight
+				position={[ 1, 3, 1 ]}
+				intensity={0.165}
+				castShadow={true}
+				decay={2}
+				penumbra={1}
+				ref={spotLightRef1}
+			/>
+			<spotLight
+				position={[ -1.2, 3, 0 ]}
+				intensity={0.165}
+				castShadow={true}
+				decay={2}
+				penumbra={1}
+			/>
+			<spotLight
+				position={[0.6, 3.0, -1.4]}
+				intensity={0.165}
+				castShadow={true}
+				decay={2}
+				penumbra={1}
+			/>
+		</Fragment>
+	);
+
+}
